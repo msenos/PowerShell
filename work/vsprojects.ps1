5eeabec56bc1d0aad2eb199160c21d1651ec45a2
Miguel Senos <miguel.senos@247tailorsteel.com>
2023-04-19 09:25:04 +0100

    selected new vs project to start

--- work/vsprojects.ps1
+++ work/vsprojects.ps1
@@ -2,11 +2,13 @@ function Start-VS {
     $max = ([System.Diagnostics.ProcessWindowStyle]::Maximized)
 
     #Visual Studio
-    $RouterPath = "C:\Users\m.senos\source\repos\TS.Router\Source\TS.Router.sln"
-    $PlannerPath = "C:\Users\m.senos\source\repos\TS.Production.Planner\source\TS.Production.Planner.Manager.Web.sln"
-    #$PurchasingPath = "C:\Users\m.senos\source\repos\TS.Purchasing\TS.Purchasing.sln"
+    #$RouterPath = "C:\Users\m.senos\source\repos\TS.Router\Source\TS.Router.sln"
+    #$PlannerPath = "C:\Users\m.senos\source\repos\TS.Production.Planner\source\TS.Production.Planner.Manager.Web.sln"
+    $PurchasingPath = "C:\Users\m.senos\source\repos\TS.Purchasing\TS.Purchasing.sln"
 
-    $pathsList = $RouterPath, $PlannerPath#, $PurchasingPath
+    $pathsList = #$RouterPath, 
+                 #$PlannerPath,
+                 $PurchasingPath
     $vs22WorkDir = "C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE"
     $vs22 = "C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE\devenv.exe"
 
