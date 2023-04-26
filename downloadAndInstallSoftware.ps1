3f9f62682e726a2315f9c97821d675386150ee01
Miguel Senos <msenos@outlook.com>
2023-04-26 18:33:13 +0100

    fixed uri's

--- downloadAndInstallSoftware.ps1
+++ downloadAndInstallSoftware.ps1
@@ -19,29 +19,33 @@ $vspro | Add-Member -MemberType NoteProperty -Name "OutFile" -Value "$env:USERPR
 #PowerToys
 $powertoys = New-Object PSObject
 $powertoys | Add-Member -MemberType NoteProperty -Name "Name" -Value "PowerToys"
-$powertoys | Add-Member -MemberType NoteProperty -Name "Uri" -Value "https://github.com/microsoft/PowerToys/releases/latest/download/PowerToysSetup.exe"
+$powertoys | Add-Member -MemberType NoteProperty -Name "Uri" -Value "https://github.com/microsoft/PowerToys/releases/download/v0.69.1/PowerToysSetup-0.69.1-x64.exe"
 $powertoys | Add-Member -MemberType NoteProperty -Name "OutFile" -Value "$env:USERPROFILE\Downloads\PowerToysSetup.exe"
 
 #ditto
 $ditto = New-Object PSObject
 $ditto | Add-Member -MemberType NoteProperty -Name "Name" -Value "ditto"
-$ditto | Add-Member -MemberType NoteProperty -Name "Uri" -Value "https://ditto-cp.sourceforge.io/DittoSetup_3_23_124_0.exe"
+$ditto | Add-Member -MemberType NoteProperty -Name "Uri" -Value "https://github.com/sabrogden/Ditto/releases/download/3.24.246.0/DittoSetup_64bit_3_24_246_0.exe"
 $ditto | Add-Member -MemberType NoteProperty -Name "OutFile" -Value "$env:USERPROFILE\Downloads\DittoSetup_latest.exe"
 
 #notepadd++
 $notepad = New-Object PSObject
 $notepad | Add-Member -MemberType NoteProperty -Name "Name" -Value "Notepad++"
-$notepad | Add-Member -MemberType NoteProperty -Name "Uri" -Value "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/latest/download/npp.*.Installer.exe"
+$notepad | Add-Member -MemberType NoteProperty -Name "Uri" -Value "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.5.2/npp.8.5.2.Installer.x64.exe"
 $notepad | Add-Member -MemberType NoteProperty -Name "OutFile" -Value "$env:USERPROFILE\Downloads\npp_latest.exe"
+
+#docker
+#git
+#SSMS e a instancia
 
 #create a list
 $appList = New-Object System.Collections.ArrayList
-#$appList.Add($postman)
-#$appList.Add($vscode)
-#$appList.Add($vspro)
-#$appList.Add($powertoys)
+$appList.Add($postman)
+$appList.Add($vscode)
+$appList.Add($vspro)
+$appList.Add($powertoys)
 $appList.Add($ditto)
-#$appList.Add($notepad)
+$appList.Add($notepad)
 
 foreach($app in $appList){
     Invoke-WebRequest -Uri $app.Uri -OutFile $app.OutFile
