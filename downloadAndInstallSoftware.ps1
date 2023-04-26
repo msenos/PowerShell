8bd477ca1a39ecfb4f49853268b804dc6c1ef05e
Miguel Senos <msenos@outlook.com>
2023-04-26 18:11:51 +0100

    clean-up

--- downloadAndInstallSoftware.ps1
+++ downloadAndInstallSoftware.ps1
@@ -1,26 +1,3 @@
-
-
-#VSProfessional
-Invoke-WebRequest -Uri "https://aka.ms/vs/17/release/vs_Professional.exe" -OutFile "$env:USERPROFILE\Downloads\vs_Professional.exe"
-Start-Process "$env:USERPROFILE\Downloads\vs_Professional.exe" -ArgumentList "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES"
-#PowerToys
-$ProgressPreference = 'SilentlyContinue'
-$PowerToysURL = 'https://github.com/microsoft/PowerToys/releases/latest/download/PowerToysSetup.exe'
-$PowerToysInstaller = "$env:USERPROFILE\Downloads\PowerToysSetup.exe"
-Invoke-WebRequest -Uri $PowerToysURL -OutFile $PowerToysInstaller
-Start-Process -FilePath $PowerToysInstaller -ArgumentList '/S' -Wait
-#ditto
-$source = "https://ditto-cp.sourceforge.io/DittoSetup_3_23_124_0.exe"
-$destination = "$env:USERPROFILE\Downloads\DittoSetup_3_23_124_0.exe"
-Invoke-WebRequest -Uri $source -OutFile $destination
-Start-Process $destination -ArgumentList "/S" -Wait
-#Notepad++
-$DownloadUrl = "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/latest/download/npp.*.Installer.exe"
-Write-Host "Downloading the latest Notepad++ to the temp folder"
-Invoke-WebRequest -Uri $DownloadUrl -OutFile "$env:TEMP\$ (Split-Path -Path $DownloadUrl -Leaf)" | Out-Null
-Write-Host "Installing the latest Notepad++"
-Start-Process -FilePath "$env:TEMP\$ (Split-Path -Path $DownloadUrl -Leaf)" -ArgumentList "/S" -Wait
-
 #postman 
 $postman = New-Object PSObject
 $postman | Add-Member -MemberType NoteProperty -Name "Name" -Value "Postman"
@@ -33,13 +10,41 @@ $vscode | Add-Member -MemberType NoteProperty -Name "Name" -Value "VS Code"
 $vscode | Add-Member -MemberType NoteProperty -Name "Uri" -Value "https://aka.ms/win32-x64-user-stable"
 $vscode | Add-Member -MemberType NoteProperty -Name "OutFile" -Value "$env:USERPROFILE\Downloads\VSCodeSetup-latest.exe"
 
+#VS Professional
+$vspro = New-Object PSObject
+$vspro | Add-Member -MemberType NoteProperty -Name "Name" -Value "VS Professional"
+$vspro | Add-Member -MemberType NoteProperty -Name "Uri" -Value "https://aka.ms/vs/17/release/vs_Professional.exe"
+$vspro | Add-Member -MemberType NoteProperty -Name "OutFile" -Value "$env:USERPROFILE\Downloads\vs_Professional.exe"
+
+#PowerToys
+$powertoys = New-Object PSObject
+$powertoys | Add-Member -MemberType NoteProperty -Name "Name" -Value "PowerToys"
+$powertoys | Add-Member -MemberType NoteProperty -Name "Uri" -Value "https://github.com/microsoft/PowerToys/releases/latest/download/PowerToysSetup.exe"
+$powertoys | Add-Member -MemberType NoteProperty -Name "OutFile" -Value "$env:USERPROFILE\Downloads\PowerToysSetup.exe"
+
+#ditto
+$ditto = New-Object PSObject
+$ditto | Add-Member -MemberType NoteProperty -Name "Name" -Value "ditto"
+$ditto | Add-Member -MemberType NoteProperty -Name "Uri" -Value "https://ditto-cp.sourceforge.io/DittoSetup_3_23_124_0.exe"
+$ditto | Add-Member -MemberType NoteProperty -Name "OutFile" -Value "$env:USERPROFILE\Downloads\DittoSetup_latest.exe"
+
+#notepadd++
+$notepad = New-Object PSObject
+$notepad | Add-Member -MemberType NoteProperty -Name "Name" -Value "Notepad++"
+$notepad | Add-Member -MemberType NoteProperty -Name "Uri" -Value "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/latest/download/npp.*.Installer.exe"
+$notepad | Add-Member -MemberType NoteProperty -Name "OutFile" -Value "$env:USERPROFILE\Downloads\npp_latest.exe"
+
 #create a list
 $appList = New-Object System.Collections.ArrayList
-$appList.Add($postman)
-$appList.Add($vscode)
+#$appList.Add($postman)
+#$appList.Add($vscode)
+#$appList.Add($vspro)
+#$appList.Add($powertoys)
+$appList.Add($ditto)
+#$appList.Add($notepad)
 
 foreach($app in $appList){
     Invoke-WebRequest -Uri $app.Uri -OutFile $app.OutFile
-    Write-Host "Installing " + $app.Name
-    Start-Process $app.OutFile -ArgumentList "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES"
+    Write-Host "Installing " $app.Name
+    #Start-Process $app.OutFile -ArgumentList "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES"
 }
