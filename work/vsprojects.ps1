b8f9ecfe20b265e8a31e3a4746fd0b5adc7a68fa
Miguel Senos <miguel.senos@247tailorsteel.com>
2023-06-29 17:56:11 +0100

    msg

--- work/vsprojects.ps1
+++ work/vsprojects.ps1
@@ -1,18 +1,36 @@
 function Run-VS {
-    $max = ([System.Diagnostics.ProcessWindowStyle]::Maximized)
-
     #Visual Studio
-    $RouterPath = "C:\Users\m.senos\source\repos\TS.Router\Source\TS.Router.sln"
-    #$PlannerPath = "C:\Users\m.senos\source\repos\TS.Production.Planner\source\TS.Production.Planner.Manager.Web.sln"
-    #$PurchasingPath = "C:\Users\m.senos\source\repos\TS.Purchasing\TS.Purchasing.sln"
+    Run-Router
+    Run-Planner
+    Run-Purchasing
+}
 
-    $pathsList = $RouterPath#, 
-                 #$PlannerPath,
-                 #$PurchasingPath
+function Run-Router {
+    $max = ([System.Diagnostics.ProcessWindowStyle]::Maximized)
+    
+    $RouterPath = "C:\Users\m.senos\source\repos\TS.Router\Source\TS.Router.sln"
     $vs22WorkDir = "C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE"
     $vs22 = "C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE\devenv.exe"
+    
+    Start-Process $vs22 -WorkingDirectory $vs22WorkDir -ArgumentList $RouterPath -WindowStyle $max
+}
 
-    foreach ($solution in $pathsList) {
-        Start-Process $vs22 -WorkingDirectory $vs22WorkDir -ArgumentList $solution -WindowStyle $max
-    }
+function Run-Planner {
+    $max = ([System.Diagnostics.ProcessWindowStyle]::Maximized)
+    
+    $PlannerPath = "C:\Users\m.senos\source\repos\TS.Production.Planner\source\TS.Production.Planner.Manager.Web.sln"
+    $vs22WorkDir = "C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE"
+    $vs22 = "C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE\devenv.exe"
+    
+    Start-Process $vs22 -WorkingDirectory $vs22WorkDir -ArgumentList $PlannerPath -WindowStyle $max
+}
+
+function Run-Purchasing {
+    $max = ([System.Diagnostics.ProcessWindowStyle]::Maximized)
+    
+    $PurchasingPath = "C:\Users\m.senos\source\repos\TS.Purchasing\TS.Purchasing.sln"
+    $vs22WorkDir = "C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE"
+    $vs22 = "C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE\devenv.exe"
+    
+    Start-Process $vs22 -WorkingDirectory $vs22WorkDir -ArgumentList $PurchasingPath -WindowStyle $max
 }
