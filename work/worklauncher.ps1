4ebd0223e5ba46102fa1bd9176569917ef5415c1
Miguel Senos <miguel.senos@247tailorsteel.com>
2023-06-29 17:48:37 +0100

    .

--- work/worklauncher.ps1
+++ work/worklauncher.ps1
@@ -1,4 +1,4 @@
 $documentsFolder = [environment]::getfolderpath("mydocuments")
+. $documentsFolder\PowerShell\Work\vsprojects.ps1
 . $documentsFolder\PowerShell\Work\planner.ps1
 . $documentsFolder\PowerShell\Work\router.ps1
-. $documentsFolder\PowerShell\Work\vsprojects.ps1
