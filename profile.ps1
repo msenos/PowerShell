25652502bf847bbdf59bd73fc83b8d1c65957f02
Miguel Senos <miguel.senos@247tailorsteel.com>
2023-11-02 16:01:05 +0000

    refactor

--- profile.ps1
+++ profile.ps1
@@ -1,6 +1,13 @@
 $documentsFolder = [environment]::getfolderpath("mydocuments")
 $isPersonalMachine = $documentsFolder.Contains("mseno")
 
+$outlook = "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE"
+$spotify = $env:APPDATA + "\Spotify\Spotify.exe"
+$teams = [Environment]::GetFolderPath([Environment+SpecialFolder]::LocalApplicationData) + "\Microsoft\Teams\Update.exe"
+$edge = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+$DockerPath = "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+
+
 . $documentsFolder\PowerShell\themes.ps1
 Write-Host "Themes loaded"
 
@@ -30,12 +37,10 @@ function Start-Utilities {
 }
 
 function Start-Outlook {
-    $outlook = "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE"
     Start-Process $outlook -WindowStyle Maximized
 }
 
 function Start-Spotify {
-    $spotify = $env:APPDATA+"\Spotify\Spotify.exe"
     Start-Process $spotify -WindowStyle Minimized
 }
 
@@ -45,27 +50,18 @@ function Start-Postman {
 
 }
 
-function Start-Slack {
-    $slack = [Environment]::GetFolderPath([Environment+SpecialFolder]::LocalApplicationData)+"\slack\slack.exe"
-    Start-Process $slack -WindowStyle Maximized
-}
-
 function Start-Teams {
-    $teams = [Environment]::GetFolderPath([Environment+SpecialFolder]::LocalApplicationData)+"\Microsoft\Teams\Update.exe"
     Start-Process $teams -ArgumentList "--processStart", "Teams.exe", "--process-start-args", "--profile=AAD" -WindowStyle Maximized
-
 }
 
 function Start-Edge {
     #Work
-    $edge = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
     Start-Process $edge --profile-directory=Default -WindowStyle Maximized
     #Personal
     #Start-Process $edge --profile-directory="Profile 1" -WindowStyle Maximized
 }
 
 function Start-Docker{
-    $DockerPath = "C:\Program Files\Docker\Docker\Docker Desktop.exe"
     Start-Process $DockerPath -WindowStyle Maximized
 }
 
