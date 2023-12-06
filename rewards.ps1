78908039f5a0509bf461d07b954a21a7f2279d1f
Miguel Senos <msenos@outlook.com>
2023-12-06 10:10:17 +0000

    clean-up

--- rewards.ps1
+++ rewards.ps1
@@ -9,20 +9,19 @@
     # Enable developer mode
     $wshell = New-Object -ComObject wscript.shell; 
     $wshell.AppActivate('Bing')
-    $wshell.SendKeys('{F12}')
-
-    # Wait for developer tools to load
-    Start-Sleep -Seconds 5
 
     # Perform a search
     for ($i = 1; $i -le 30; $i++){
-        # $number = Get-Random -Minimum 10 -Maximum 101
+        $number = Get-Random -Minimum 10 -Maximum 101
         $wshell.SendKeys('{F6}')
         Start-Sleep -MilliSeconds 100
-        $wshell.SendKeys($i)
+        $wshell.SendKeys('{F6}')
+        Start-Sleep -MilliSeconds 100
+        $wshell.SendKeys($number)
         Start-Sleep -MilliSeconds 100
         $wshell.SendKeys('{ENTER}')
-        Write-Host($i)
+        $result = "$i - $number"
+        Write-Host($result)
         
         Start-Sleep -Seconds 10
     }
