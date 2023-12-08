98c28907560806159743ee68b15bcf59a365c81e
Miguel Senos <msenos@outlook.com>
2023-12-08 08:25:03 +0000

    new random function

--- rewards.ps1
+++ rewards.ps1
@@ -11,16 +11,16 @@
     $wshell.AppActivate('Bing')
 
     # Perform a search
+    $numbers = 1..1000 | Get-Random -Count 30 -Unique
     for ($i = 1; $i -le 30; $i++){
-        $number = Get-Random -Minimum 10 -Maximum 101
         $wshell.SendKeys('{F6}')
         Start-Sleep -MilliSeconds 100
         $wshell.SendKeys('{F6}')
         Start-Sleep -MilliSeconds 100
-        $wshell.SendKeys($number)
+        $wshell.SendKeys($numbers[$i])
         Start-Sleep -MilliSeconds 100
         $wshell.SendKeys('{ENTER}')
-        $result = "$i - $number"
+        $result = "$i - $numbers[$i]"
         Write-Host($result)
         
         Start-Sleep -Seconds 10
