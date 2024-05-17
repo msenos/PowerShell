c3d6c0fc56d7c4a5cd885e56311b295981ea8aa7
Miguel Senos <miguel.senos@247tailorsteel.com>
2024-05-17 18:01:01 +0200

    removed unsed files

--- work/vsprojects.ps1
+++ work/vsprojects.ps1
@@ -6,57 +6,37 @@ $reposPath        = "C:\Users\m.senos\source\repos"
 $RouterPath       = $reposPath + "\TS.Router\Source\TS.Router.sln"
 $PlannerPath      = $reposPath + "\TS.Production.Planner\source\TS.Production.Planner.Manager.Web.sln"
 $PurchasingPath   = $reposPath + "\TS.Purchasing\TS.Purchasing.sln"
-# $NesterPath       = $reposPath + "\TS.Operations.Nester\src\TS.Operations.Nester.sln"
-# $MaterialPath     = $reposPath + "\TS.Operations.Material\src\TS.Operations.Material.sln"
-# $ProdCustomerPath = $reposPath + "\TS.Production.Customer\src\TS.Production.Customer.sln"
 
 function VS-Start {
     ##Visual Studio
     Repos-Update
     
+    Start-Sleep -Seconds 1
     Router-Start
     Start-Sleep -Seconds 1
     Planner-Start
     Start-Sleep -Seconds 1
     Purchasing-Start
-    #Nester-Start
-    #Material-Start
 }
 function Repos-Update{
     Router-Update
     Planner-Update
     Purchasing-Update
-    # Nester-Update
-    # Material-Update
-    # Production-Update
-    # ProdCustomer-Update
 }
 function Repos-Clean{
     Router-Clean
     Planner-Clean
     Purchasing-Clean
-    # Nester-Clean
-    # Material-Clean
-    # Production-Clean
-    # ProdCustomer-Clean
 }
 function Repos-Build{
     Router-Build
     Planner-Build
     Purchasing-Build
-    # Nester-Build
-    # Material-Build
-    # Production-Build
-    # ProdCustomer-Build
 }
 function Repos-Rebuild{
     Router-Rebuild
     Planner-Rebuild
     Purchasing-Rebuild
-    # Nester-Rebuild
-    # Material-Rebuild
-    # Production-Rebuild
-    # ProdCustomer-Rebuild
 }
 function Router-Start {
     Start-Process $vs22 -WorkingDirectory $vs22WorkDir -ArgumentList $RouterPath -WindowStyle $max
@@ -67,12 +47,3 @@ function Planner-Start {
 function Purchasing-Start {
     Start-Process $vs22 -WorkingDirectory $vs22WorkDir -ArgumentList $PurchasingPath -WindowStyle $max
 }
-# function Nester-Start {
-#     Start-Process $vs22 -WorkingDirectory $vs22WorkDir -ArgumentList $NesterPath -WindowStyle $max
-# }
-# function Material-Start {
-#     Start-Process $vs22 -WorkingDirectory $vs22WorkDir -ArgumentList $MaterialPath -WindowStyle $max
-# }
-# function ProdCustomer-Start {
-#     Start-Process $vs22 -WorkingDirectory $vs22WorkDir -ArgumentList $ProdCustomerPath -WindowStyle $max
-# }
--- work/workloader.ps1
+++ work/workloader.ps1
@@ -3,7 +3,3 @@ $documentsFolder = [environment]::getfolderpath("mydocuments")
 . $documentsFolder\PowerShell\Work\router.ps1
 . $documentsFolder\PowerShell\Work\planner.ps1
 . $documentsFolder\PowerShell\Work\purchasing.ps1
-# . $documentsFolder\PowerShell\Work\operations.material.ps1
-# . $documentsFolder\PowerShell\Work\operations.nester.ps1
-# . $documentsFolder\PowerShell\Work\production.ps1
-# . $documentsFolder\PowerShell\Work\production.customer.ps1
