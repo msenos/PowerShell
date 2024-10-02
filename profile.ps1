d5113b75b4195dd732f7d86c98db681b6ffdc447
Miguel Senos <miguel.senos@247tailorsteel.com>
2024-10-02 15:51:58 +0200

    small refactor

--- profile.ps1
+++ profile.ps1
@@ -1,16 +1,50 @@
+function Start-Work {
+    Start-Edge
+    Start-Spotify
+    Start-Outlook
+    Start-Postman
+    Start-Docker
+    Start-VS
+}
+function Start-Outlook {
+    $outlook = "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE"
+    Start-Process $outlook -WindowStyle Maximized
+}
+function Start-Spotify {
+    $spotify = $env:APPDATA + "\Spotify\Spotify.exe"
+    Start-Process $spotify -WindowStyle Minimized
+}
+function Start-Postman {
+    $postman = [Environment]::GetFolderPath([Environment+SpecialFolder]::LocalApplicationData)+"\Postman\Postman.exe"
+    Start-Process $postman -WindowStyle Maximized
+}
+function Start-Edge {
+    $edge         = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+    $edgeWork     = $edge + " - Work"
+    $edgePersonal = $edge + " - Personal"
+    
+    #Work
+    Start-Process $edgeWork -WindowStyle Maximized
+    #Personal
+    Start-Process $edgePersonal -WindowStyle Maximized
+}
+function Start-Docker {
+    $docker = "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+    Start-Process $docker -WindowStyle Maximized
+}
+function Shutdown {
+    Stop-Computer
+}
+function Restart {
+    Restart-Computer
+}
+
+#### Execution ####
 $location     = "C:\Users\mseno"
 $machineUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 $isWorkMachine = !$machineUser.Contains("mseno")
 $documentsFolder = $isWorkMachine ? "C:\Users\m.senos\Documents" : [environment]::getfolderpath("mydocuments") 
 
-$outlook      = "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE"
-$docker       = "C:\Program Files\Docker\Docker\Docker Desktop.exe"
-$edge         = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
-$edgeWork     = $edge + " - Work"
-$edgePersonal = $edge + " - Personal"
-$spotify      = $env:APPDATA + "\Spotify\Spotify.exe"
-
-#### Execution ####
 . $documentsFolder\PowerShell\themes.ps1
 Write-Host "Themes loaded"
 . $documentsFolder\PowerShell\rewards.ps1
@@ -24,42 +58,8 @@ if($isWorkMachine){
     . $documentsFolder\PowerShell\Work\purchasing.ps1
     }
 
-Write-Host "Startup complete"
+Write-Host "Powershell setup complete"
 
 Set-Location -Path $location
 
 #### End Execution ####
-
-function Start-Work {
-    Start-Edge
-    Start-Spotify
-    Start-Outlook
-    Start-Postman
-    Start-Docker
-    Start-VS
-}
-function Start-Outlook {
-    Start-Process $outlook -WindowStyle Maximized
-}
-function Start-Spotify {
-    Start-Process $spotify -WindowStyle Minimized
-}
-function Start-Postman {
-    $postman = [Environment]::GetFolderPath([Environment+SpecialFolder]::LocalApplicationData)+"\Postman\Postman.exe"
-    Start-Process $postman -WindowStyle Maximized
-}
-function Start-Edge {
-    #Work
-    Start-Process $edgeWork -WindowStyle Maximized
-    #Personal
-    Start-Process $edgePersonal -WindowStyle Maximized
-}
-function Start-Docker {
-    Start-Process $docker -WindowStyle Maximized
-}
-function Shutdown {
-    Stop-Computer
-}
-function Restart {
-    Restart-Computer
-}
