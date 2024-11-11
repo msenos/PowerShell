53e39fe7c7f97c85896647d35f26ecba1637edfc
Miguel Senos <miguel.senos@infinit.com>
2024-11-11 10:25:45 +0000

    removed unused files

--- profile.ps1
+++ profile.ps1
@@ -1,37 +1,37 @@
 function Start-Work {
-    Start-Edge
-    Start-Spotify
-    Start-Outlook
-    Start-Postman
-    Start-Docker
-    Start-VS
+    # Start-Edge
+    # Start-Spotify
+    # Start-Outlook
+    # Start-Postman
+    # Start-Docker
+    # Start-VS
 }
-function Start-Outlook {
-    $outlook = "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE"
-    Start-Process $outlook -WindowStyle Maximized
-}
+# function Start-Outlook {
+#     $outlook = "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE"
+#     Start-Process $outlook -WindowStyle Maximized
+# }
 function Start-Spotify {
     $spotify = $env:APPDATA + "\Spotify\Spotify.exe"
     Start-Process $spotify -WindowStyle Minimized
 }
-function Start-Postman {
-    $postman = [Environment]::GetFolderPath([Environment+SpecialFolder]::LocalApplicationData)+"\Postman\Postman.exe"
-    Start-Process $postman -WindowStyle Maximized
-}
-function Start-Edge {
-    $edge         = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
-    $edgeWork     = $edge + " - Work"
-    $edgePersonal = $edge + " - Personal"
+# function Start-Postman {
+#     $postman = [Environment]::GetFolderPath([Environment+SpecialFolder]::LocalApplicationData)+"\Postman\Postman.exe"
+#     Start-Process $postman -WindowStyle Maximized
+# }
+# function Start-Edge {
+#     $edge         = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+#     $edgeWork     = $edge + " - Work"
+#     $edgePersonal = $edge + " - Personal"
     
-    #Work
-    Start-Process $edgeWork -WindowStyle Maximized
-    #Personal
-    Start-Process $edgePersonal -WindowStyle Maximized
-}
-function Start-Docker {
-    $docker = "C:\Program Files\Docker\Docker\Docker Desktop.exe"
-    Start-Process $docker -WindowStyle Maximized
-}
+#     #Work
+#     Start-Process $edgeWork -WindowStyle Maximized
+#     #Personal
+#     Start-Process $edgePersonal -WindowStyle Maximized
+# }
+# function Start-Docker {
+#     $docker = "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+#     Start-Process $docker -WindowStyle Maximized
+# }
 function Shutdown {
     Stop-Computer
 }
@@ -40,23 +40,11 @@ function Restart {
 }
 
 #### Execution ####
-$location     = "C:\Users\mseno"
-$machineUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
-$isWorkMachine = !$machineUser.Contains("mseno")
-$documentsFolder = $isWorkMachine ? "C:\Users\m.senos\Documents" : [environment]::getfolderpath("mydocuments") 
-
+$documentsFolder = "C:\Users\mseno\OneDrive\Documents\"
 . $documentsFolder\PowerShell\themes.ps1
 Write-Host "Themes loaded"
 . $documentsFolder\PowerShell\rewards.ps1
 Write-Host "Rewards loaded"
-
-if($isWorkMachine){
-    $location = "C:\Users\m.senos"
-    . $documentsFolder\PowerShell\Work\vsprojects.ps1
-    . $documentsFolder\PowerShell\Work\router.ps1
-    . $documentsFolder\PowerShell\Work\planner.ps1
-    . $documentsFolder\PowerShell\Work\purchasing.ps1
-    }
 
 Write-Host "Powershell setup complete"
 
--- work/vsprojects.ps1
+++ work/vsprojects.ps1
@@ -5,30 +5,16 @@ function Start-VS {
     #Router
     Start-Sleep -Seconds 1
     Router-Start
-    #Planner
-    Start-Sleep -Seconds 1
-    Planner-Start
-    #Purchasing
-    Start-Sleep -Seconds 1
-    Purchasing-Start
 }
 function Repos-Update{
     Router-Update
-    Planner-Update
-    Purchasing-Update
 }
 function Repos-Clean{
     Router-Clean
-    Planner-Clean
-    Purchasing-Clean
 }
 function Repos-Build{
     Router-Build
-    Planner-Build
-    Purchasing-Build
 }
 function Repos-Rebuild{
     Router-Rebuild
-    Planner-Rebuild
-    Purchasing-Rebuild
 }
